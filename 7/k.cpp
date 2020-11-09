76861bec076c504bbf26a1a2706c3cfcc49196a3
George <egor.kondratev2001@mail.ru>
2020-11-10 00:29:40 +0300

    Create k.cpp

---
+++ 7/k.cpp
@@ -0,0 +1,1054 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct len
+{
+	ll l, r;
+};
+bool qq(len &lhs, len &rhs)
+{
+	if (lhs.r != rhs.r)
+		return (lhs.r < rhs.r);
+	else
+		return (lhs.l < rhs.l);
+}
+int main()
+{
+	ll n;
+	cin >> n;
+	vector<len> v(n);
+	for (int i = 0; i < n; ++i)
+		cin >> v[i].l >> v[i].r;
+	sort(v.begin(), v.end(), qq);
+	ll rr = v[0].l, ans = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (rr <= v[i].l)
+		{
+			ans++;
+			rr = v[i].r;
+		}
+	}
+	cout << ans;
+	return 0;
+}
