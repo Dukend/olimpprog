ec1605b644e260b08cec49bb5d11d90d02ac9719
George <egor.kondratev2001@mail.ru>
2020-11-15 12:32:36 +0300

    Update i.cpp

--- 10/i.cpp
+++ 10/i.cpp
@@ -1,19 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp(int x, int y)
-{
-	int a = max(x, y);
-	int b = min(x, y);
-	if (x % 2 != 0 && y % 2 != 0)
-		return 0;
-	if (a % 2 == 0)
-		if (b % 2 == 0)
-			return 2;
-		else
-			return 1;
-	return 3;
-}
-
 int main()
 {
 	int n, ans = 0;
@@ -22,7 +8,14 @@ int main()
 	{
 		int x, y;
 		cin >> x >> y;
-		ans ^= dp(x, y);
+		if (x % 2 != 1 || y % 2 != 1)
+			if (max(x, y) % 2 == 0)
+				if (min(x, y) % 2 == 0)
+					ans ^= 2;
+				else
+					ans ^= 1;
+			else
+				ans ^= 3;
 	}
 	if (ans)
 		cout << 1;
