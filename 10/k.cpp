d54c8ac7c8617088cc73402122350023a97af0af
George <egor.kondratev2001@mail.ru>
2020-11-15 12:32:41 +0300

    Create k.cpp

---
+++ 10/k.cpp
@@ -0,0 +1,918 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, l, r, ans = 0;
+	cin >> n >> m >> l >> r;
+	ans = r - l - 1;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> l >> r;
+		ans ^= r - l - 1;
+	}
+	if (ans)
+		cout << 1;
+	else
+		cout << 2;
+	return 0;
+}
