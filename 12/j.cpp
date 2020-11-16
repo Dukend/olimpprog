22da3425d7f0122bf8c0c7636feaaddc081e996f
George <egor.kondratev2001@mail.ru>
2020-11-16 15:43:56 +0300

    Create j.cpp

---
+++ 12/j.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long MOD = 4294967296;
+int main()
+{
+	long long n;
+	cin >> n;
+	long long q;
+	vector<long long> v(n);
+	for (int i = 0; i < n; i++)
+	{
+	}
+	cout << q;
+	return 0;
+}
