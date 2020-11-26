18ed481f869c159ce393aa9bf8bdff7bc7af3e40
George <egor.kondratev2001@mail.ru>
2020-11-26 23:26:02 +0300

    Create d.cpp

---
+++ fight/d.cpp
@@ -0,0 +1,460 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+
+		if (((a - b) % 2 == 1) || ((b - 1) % 2 == 1))
+			cout << "Alice";
+		else
+			cout << "Bob";
+		cout << endl;
+	}
+
+	return 0;
+}
